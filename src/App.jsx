cd5be0b4e422eb3cf5fb25a758701dc475f29992
Jarjajarr <charatvc@gmail.com>
2023-10-06 17:52:15 +0700

    fixed src bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
 import Me from './pages/Me'
 import Project from './pages/Project'
 import Contact from './pages/Contact'
